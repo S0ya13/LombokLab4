e9607b915d1d49e21b7863dea9e6e955f2c6d7e4
S0ya <oj.savchak@gmail.com>
2025-12-16 15:49:31 +0100

    Created setters.

--- src/main/java/vistula/car/Car.java
+++ src/main/java/vistula/car/Car.java
@@ -35,5 +35,24 @@ public class Car {
     public int getQuantity() {
         return quantity;
     }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
 

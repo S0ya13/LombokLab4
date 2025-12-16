9bf82f4e7674813b02c61db7180cd875d3bd06b2
S0ya <oj.savchak@gmail.com>
2025-12-16 15:37:28 +0100

    Created variables and constructor.

--- src/main/java/vistula/car/Car.java
+++ src/main/java/vistula/car/Car.java
@@ -1,5 +1,20 @@
 package vistula.car;
 
 public class Car {
+    private String model;
+    private String brand;
+    private int year;
+    private int price;
+    private String color;
+    private int quantity;
 
+    public Car(String model, String brand, int year, int price, String color, int quantity) {
+        this.model = model;
+        this.brand = brand;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.quantity = quantity;
+    }
 }
+
